b703c995a6d6221c5b856fc1ae46dbe2a2e31533
Elizar Gimenez <141979161+Razile623@users.noreply.github.com>
2025-01-06 13:54:54 +0800

    Add files via upload

---
+++ LatLonLocalSignificantSites_0.js
@@ -0,0 +1,561 @@
+var json_LatLonLocalSignificantSites_0 = {
+  type: "FeatureCollection",
+  name: "LatLonLocalSignificantSites_0",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        ID: "2001",
+        "Property Name": "Gusaling Yap Anton | Yap Anton Building",
+        Latitude: "10.29728946",
+        Longitude: "123.9078304",
+        classification: "LocalCulturalProperties"
+      },
+      geometry: { type: "Point", coordinates: [123.9078304, 10.29728946] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2002",
+        "Property Name":
+          "Kinalalagyan ng Paaralan ng Silangang Visayas | East Visayan Academy Grounds",
+        Latitude: "10.27255734",
+        Longitude: "123.8460047",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8460047, 10.27255734] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2003",
+        "Property Name":
+          "Sementeryong Bayan ng San Jose Manggagawa | Saint Joseph the Worker Public Cemetery",
+        Latitude: "10.25288436",
+        Longitude: "123.8585512",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8585512, 10.25288436] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2004",
+        "Property Name": "Museo ng Canton | Canton Museum",
+        Latitude: "10.24980409",
+        Longitude: "123.8438461",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8438461, 10.24980409] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2005",
+        "Property Name": "Bantayog ng Tres Ali�os | Tres Ali�os Monument",
+        Latitude: "10.24647527",
+        Longitude: "123.8477524",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8477524, 10.24647527] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2006",
+        "Property Name":
+          "Bantayog ng mga Pambansang Bayani | National Heroes Monument",
+        Latitude: "10.24545938",
+        Longitude: "123.8482778",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8482778, 10.24545938] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2007",
+        "Property Name":
+          "Kolehiyo ng Lungsod ng Talisay | Talisay City College",
+        Latitude: "10.24579723",
+        Longitude: "123.8486211",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8486211, 10.24579723] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2008",
+        "Property Name":
+          "Liwasan ng Kalayaan at Bantayog ng Pagdaong | Liberation Park and Landing Monument",
+        Latitude: "10.241691",
+        Longitude: "123.849218",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.849218, 10.241691] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2009",
+        "Property Name":
+          "Sementeryong Bayan ng Poblacion | Poblacion Public Cemetery",
+        Latitude: "10.24370254",
+        Longitude: "123.8434337",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8434337, 10.24370254] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2010",
+        "Property Name":
+          "Lumang Bantayog at Liwasan ni Bonifacio | Old Bonifacio Monument and Park",
+        Latitude: "10.21115736",
+        Longitude: "123.7592102",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.7592102, 10.21115736] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2011",
+        "Property Name":
+          "Lumang Gusali ng Bahay-Pamahalaan ng Naga (Cebu) | Old Municipal Hall Building of Naga (Cebu)",
+        Latitude: "10.21106176",
+        Longitude: "123.7594084",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.7594084, 10.21106176] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2012",
+        "Property Name":
+          "Hospisyo ng San Jose ng Barili |  Hospicio de San Jose de Barili",
+        Latitude: "10.11572263",
+        Longitude: "123.4937023",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4937023, 10.11572263] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2013",
+        "Property Name":
+          "Gusaling Gabaldon ng Paaralang Sentral ng Barili | Barili Central School Gabaldon Building",
+        Latitude: "10.11689018",
+        Longitude: "123.5110547",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.5110547, 10.11689018] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2014",
+        "Property Name": "Bahay Hilarion Alquizola | Hilarion Alquizola House",
+        Latitude: "10.11574787",
+        Longitude: "123.5116635",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.5116635, 10.11574787] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2015",
+        "Property Name": "Obelisk ng Gorordo | Gorodo Obelisk",
+        Latitude: "10.11415051",
+        Longitude: "123.5112576",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.5112576, 10.11415051] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2016",
+        "Property Name": "Bantayog ng Santa Ana ",
+        Latitude: "10.1144006",
+        Longitude: "123.5109856",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.5109856, 10.1144006] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2017",
+        "Property Name": "Pungtod Cemetery",
+        Latitude: "9.570088678",
+        Longitude: "123.4634865",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4634865, 9.570088678] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2018",
+        "Property Name": "Saint Joseph the Carpenter Parish Church",
+        Latitude: "9.562911706",
+        Longitude: "123.4573328",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4573328, 9.562911706] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2019",
+        "Property Name": "Saint Joseph Shrine and Church Plaza",
+        Latitude: "9.564750169",
+        Longitude: "123.4572853",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4572853, 9.564750169] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2020",
+        "Property Name": "Nueva Caceres Elementary School, H.E. Building",
+        Latitude: "9.560072719",
+        Longitude: "123.4538927",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4538927, 9.560072719] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2021",
+        "Property Name": "Balwarte ng Bonbon | Bonbon Baluarte",
+        Latitude: "9.557157696",
+        Longitude: "123.4540193",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4540193, 9.557157696] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2022",
+        "Property Name": "Looc Watchtower",
+        Latitude: "9.540548927",
+        Longitude: "123.4447159",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4447159, 9.540548927] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2023",
+        "Property Name":
+          "Oslob Central Elementary School including Admin Building, H.E. Building, and Acacia Trees",
+        Latitude: "9.524796713",
+        Longitude: "123.4338214",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4338214, 9.524796713] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2024",
+        "Property Name": "First Catholic Cemetery",
+        Latitude: "9.523138718",
+        Longitude: "123.4359994",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4359994, 9.523138718] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2025",
+        "Property Name": "Cemetery Walls and Gates",
+        Latitude: "9.522618988",
+        Longitude: "123.4357359",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4357359, 9.522618988] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2026",
+        "Property Name":
+          "Bantayan sa Poblacion | Poblacion Baluarte (Watchtower)",
+        Latitude: "9.520876685",
+        Longitude: "123.4354761",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4354761, 9.520876685] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2027",
+        "Property Name": "Church Plaza",
+        Latitude: "9.520754377",
+        Longitude: "123.4344063",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4344063, 9.520754377] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2028",
+        "Property Name": "Saint Mary's Academy Old Building",
+        Latitude: "9.521440903",
+        Longitude: "123.4340443",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4340443, 9.521440903] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2029",
+        "Property Name": "Pamanang Liwasang | Bayan Municipal Heritage Park",
+        Latitude: "9.520706765",
+        Longitude: "123.4331068",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4331068, 9.520706765] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2030",
+        "Property Name": "Waiting Chapel (Prayer Room)",
+        Latitude: "9.520554614",
+        Longitude: "123.4345918",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4345918, 9.520554614] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2031",
+        "Property Name": "Cuartel Structure",
+        Latitude: "9.520224767",
+        Longitude: "123.4341915",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4341915, 9.520224767] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2032",
+        "Property Name": "Simbahan ng UCCP | UCCP Church",
+        Latitude: "9.52002752",
+        Longitude: "123.4334367",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4334367, 9.52002752] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2033",
+        "Property Name": "Immaculate Conception Parish Church",
+        Latitude: "9.521121358",
+        Longitude: "123.4338213",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4338213, 9.521121358] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2034",
+        "Property Name":
+          "Bahay ni Don Jose Tio Bonpua, Sr. at ang Museo ng Banal na Sining | Don Jose Tio Bonpua, Sr. House and the Museum of the Sacred Art, Inc.",
+        Latitude: "9.51935369",
+        Longitude: "123.4307534",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4307534, 9.51935369] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2035",
+        "Property Name": "Daanglungsod Fortress",
+        Latitude: "9.513667023",
+        Longitude: "123.4231839",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4231839, 9.513667023] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2036",
+        "Property Name":
+          "Daanglungsod Estaca - Kuta | Daanglungsod Estaca - Fortress",
+        Latitude: "9.513317639",
+        Longitude: "123.4224655",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4224655, 9.513317639] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2037",
+        "Property Name": "Daanglungsod Baluartes (Watchtowers)",
+        Latitude: "9.512746065",
+        Longitude: "123.4222335",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.4222335, 9.512746065] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2038",
+        "Property Name": "Santander Municipal Hall",
+        Latitude: "9.417252155",
+        Longitude: "123.3350834",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.3350834, 9.417252155] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2039",
+        "Property Name": "Saint Gabriel Archangel Parish of Santander",
+        Latitude: "9.416066433",
+        Longitude: "123.3354435",
+        classification: "LocalCulturalProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.3354435, 9.416066433] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2040",
+        "Property Name": "Century-old Bell Tower or Belfry",
+        Latitude: "9.415923794",
+        Longitude: "123.3352599",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.3352599, 9.415923794] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2347",
+        "Property Name": "Port of Entry | Liloan",
+        Latitude: "9.416536556",
+        Longitude: "123.3028175",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.3028175, 9.416536556] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2344",
+        "Property Name": "Watchtower or Bantayan sa Hari/Baluarte | Liloan",
+        Latitude: "9.417814832",
+        Longitude: "123.30116",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.30116, 9.417814832] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2346",
+        "Property Name": "Port of Entry | Talisay",
+        Latitude: "9.459150106",
+        Longitude: "123.3008606",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.3008606, 9.459150106] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2048",
+        "Property Name": "Parish Church of Santa Rosa de Lima of Daanbantayan",
+        Latitude: "11.2504859",
+        Longitude: "123.9999819",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.9999819, 11.2504859] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: "2060",
+        "Property Name":
+          "Parish Church of Santa Teresa de Avila, Talisay, Cebu",
+        Latitude: "10.2438679",
+        Longitude: "123.8478523",
+        classification: "RegisteredProperties"
+
+      },
+      geometry: { type: "Point", coordinates: [123.8478523, 10.2438679] },
+    },
+  ],
+};
